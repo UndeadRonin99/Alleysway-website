34c62fd8f2282b4785f5b524ed49174ac8e38445
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2024-09-06 10:30:56 +0200

    Merging to main

--- XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
+++ XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c1a0c81639c36b633ebf8cc439201b5b7ffbe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6672fca29aa23129a43b76140179d1a047be1b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
