28005aff62669efd11a57d5d8b73c5e82c9efd2d
TylerFriedman <st1016340@vcconnect.edu.za>
2024-09-08 14:55:15 +0200

    for nic

--- XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
+++ XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db11956a8bfe78a4d6848dd7be3915d712a3166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9d03412285d95a13ecc81257fcfb0987804627")]
 [assembly: System.Reflection.AssemblyProductAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
