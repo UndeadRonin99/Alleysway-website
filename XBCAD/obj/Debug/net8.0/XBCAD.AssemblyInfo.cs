38407ae940b0e60a33d32e8e422d72af121ed007
Matteo Pita <ST10024454@vcconnect.edu.za>
2024-09-10 15:07:26 +0200

    Staging

--- XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
+++ XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e605c8cf7b724fe3e935465957804540203c8d80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861c00450d78e33c92099c7a27aba46e977c004b")]
 [assembly: System.Reflection.AssemblyProductAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
