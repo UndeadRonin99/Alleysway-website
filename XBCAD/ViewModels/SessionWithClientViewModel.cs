ef055701e9bdc574b1e3d54ab8616f88dd73f19e
ShiraBome <104002460+ShiraBome@users.noreply.github.com>
2024-11-19 15:55:55 +0200

    SessionWithClientViewModel is Updated

--- XBCAD/ViewModels/SessionWithClientViewModel.cs
+++ XBCAD/ViewModels/SessionWithClientViewModel.cs
@@ -1,9 +1,12 @@
 ﻿// In ViewModels/SessionWithClientViewModel.cs
 namespace XBCAD.ViewModels
 {
+    // ViewModel class for representing a session along with the associated client.
     public class SessionWithClientViewModel
     {
+        // Represents the details of the booked session.
         public BookedSession Session { get; set; }
+         // Represents the client associated with the session.
         public ClientViewModel Client { get; set; }
     }
 }
