5b431a19feeadd93cbe09dfe889487808c93f263
ShiraBome <104002460+ShiraBome@users.noreply.github.com>
2024-11-19 15:54:45 +0200

    Updated  trainer session

--- XBCAD/ViewModels/TrainerSessionsViewModel.cs
+++ XBCAD/ViewModels/TrainerSessionsViewModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace XBCAD.ViewModels
 {
+    // ViewModel class for displaying a trainer's sessions in the application.
     public class TrainerSessionsViewModel
     {
+        // Represents the trainer whose sessions are being managed or displayed.
         public Trainer Trainer { get; set; }
+        // List of booked sessions for the trainer.
+        // Each session is an instance of the BookedSession class that contains session-specific details.
         public List<BookedSession> Sessions { get; set; }
     }
 
